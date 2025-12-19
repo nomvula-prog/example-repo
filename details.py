439c71c89f60000a129c44f1b832e1e9a491700a
nomvula <nomvulapta@gmail.com>
2025-12-19 13:00:13 +0200

    Add files via upload

---
+++ details.py
@@ -0,0 +1,9 @@
+name = input("Name:")
+age = input("Age:")
+house_number = input("House number:")
+street_name = input("Street name:")
+
+print("Your name is " + name + " your are " + age +" old" + " live in " + house_number + " " + street_name)
+
+"""print(f"This is {name}.They are {age} years old and live at house number\
+       {house_number} on {street_name}.")"""
